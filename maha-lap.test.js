b9acee18acd0833a4e808949cc60e752d630a49b
pomelooooooo <suwijak1691@gmail.com>
2019-09-22 12:50:03 +0700

    add

--- maha-lap.test.js
+++
@@ -1,9 +0,0 @@
-const Mahalap = require("./maha-lap");
-
-describe("Test Maha-Lap", ()=> {
-    test("Test Tamnai with day",() => {
-        const result = Mahalap.tamnaiWithDay("จันทร์");
-        expect(result).toEqual(2);
-
-    });
-});
